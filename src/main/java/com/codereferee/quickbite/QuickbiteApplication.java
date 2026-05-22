e39f1bdff6736d00b5691ca45d7cc449cfcc549a
phdcoco <claireluna470@gmail.com>
2026-05-22 18:19:11 +0900

    feat: initialize

---
+++ src/main/java/com/codereferee/quickbite/QuickbiteApplication.java
@@ -0,0 +1,13 @@
+package com.codereferee.quickbite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickbiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickbiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codereferee/quickbite/QuickbiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codereferee.quickbite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickbiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
